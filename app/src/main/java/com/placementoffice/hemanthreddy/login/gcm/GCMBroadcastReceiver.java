e2f3b7223be78de1ac8335b956092bb44b643d63
hemanth reddy <hemanth5636@gmail.com>
2016-03-21 10:28:23 +0530

    Update GCMBroadcastReceiver.java

--- app/src/main/java/com/placementoffice/hemanthreddy/login/gcm/GCMBroadcastReceiver.java
+++ app/src/main/java/com/placementoffice/hemanthreddy/login/gcm/GCMBroadcastReceiver.java
@@ -6,9 +6,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.gcm.GcmListenerService;
 import com.placementoffice.hemanthreddy.login.HomeScreen;
+import com.placementoffice.hemanthreddy.login.MsgSharedPreference;
+
+import org.json.JSONObject;
 
 /**
  * Created by hemanthreddy on 2/24/2016.
@@ -21,20 +26,33 @@ public class GCMBroadcastReceiver extends GcmListenerService {
        // startWakefulService(context,(intent.setComponent(componentName)));
        // setResultCode(Activity.RESULT_OK);
     //}
-
+//l;kjldijbgimdglvlkblgidjiobld
+    int x,a;
     NotificationUtils notificationUtils;
 
     @Override
     public void onMessageReceived(String from, Bundle bundle)
     {
-        String title = bundle.getString("title");
-        String message  = bundle.getString("message");
 
+        String title = bundle.getString("title");
+        String don = bundle.getString("don");
+        String id = bundle.getString("id");
+        //String message  = bundle.getString("data");
+        Log.e("msg came","fuck u");
+        MsgSharedPreference obj = new MsgSharedPreference(getApplicationContext());
+        obj.setMessage(title,don,id);
+        obj.setIsMsgReceived(true);
+        Log.e("ismsg",""+obj.isMsgReceived());
         Intent resultIntent = new Intent(getApplicationContext(), HomeScreen.class);
-        resultIntent.putExtra("message",message);
+        resultIntent.putExtra("title", title);
+        resultIntent.putExtra("don",don);
+        resultIntent.putExtra("id",id);
+
+        Log.e("title",title);
+        Log.e("from",from);
 
         notificationUtils = new NotificationUtils(getApplicationContext());
         resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        notificationUtils.showNotification(title,message,resultIntent);
+        notificationUtils.showNotification("PO Notice",title,resultIntent);
     }
 }
